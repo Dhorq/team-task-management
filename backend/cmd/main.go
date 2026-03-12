e9bab01bfcf39f91a203ac20b114fafaa5d48bb9
Dhorq <ridhorifqy34@gmail.com>
2026-03-12 20:52:09 +0700

    feat: add automigrate db from gorm and setup all routes in main.go

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -4,21 +4,22 @@ import (
 	"net/http"
 
 	"github.com/Dhorq/team-task-management/models"
+	"github.com/Dhorq/team-task-management/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	models.ConnectDB()
+	models.DB.AutoMigrate(&models.User{})
 
 	router := gin.Default()
+	routes.SetupRoutes(router)
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-
-	// router.GET("/task", getTask)
 
 	router.Run(":5000")
 }
